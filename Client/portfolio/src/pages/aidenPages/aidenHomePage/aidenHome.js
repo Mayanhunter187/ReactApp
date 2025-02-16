199df1cd1de90c249f3a3f11e7172a3ee999646a
Aiden <aiden.j.dunaway@gmail.com>
2025-02-15 21:59:58 -0500

    Aiden Profile
    Made some changes to Aiden Profile

--- Client/portfolio/src/pages/aidenPages/aidenHomePage/aidenHome.js
+++ Client/portfolio/src/pages/aidenPages/aidenHomePage/aidenHome.js
@@ -4,14 +4,15 @@ import './aidenHome.css';
 
 
 
-
 function AidenHome() {
   return (
     <div>
       <header className="home-header">
       <h1><Link to = "/"><button className="standardButton">Return to home page</button></Link></h1>   
+      <h2><Link to = "https://google.com"><button className="standardButton">Google</button></Link></h2>
+      <h2><Link to = "https://www.linkedin.com/in/aiden-dunaway"><button className="standardButton">Linkedin</button></Link></h2>
       </header>
       </div>
   );
 }
-export default AidenHome;
+export default AidenHome; 
