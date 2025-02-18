7a56557135792d65509e97d2ad3776a7c7d4dfc7
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2025-02-18 13:08:56 -0500

    test

--- Client/portfolio/src/pages/kianPages/kianHomePage/kianHome.js
+++ Client/portfolio/src/pages/kianPages/kianHomePage/kianHome.js
@@ -2,13 +2,11 @@
 import { Link } from 'react-router-dom';
 import './kianHome.css';
 
-
-
 function KianHome() {
   return (
     <div>
       <header className="home-header">
-      <h1><Link to = "/"><button className="standardButton">Return to home page</button></Link></h1>   
+      <h1><Link to = "/"><button className="standardButton">Return to the home page</button></Link></h1>   
       </header>
       </div>
   );
