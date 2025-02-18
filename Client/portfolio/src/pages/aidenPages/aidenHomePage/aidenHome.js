42e8a5825696f766a3a6ab238c6424f3dd96689f
Aiden <aiden.j.dunaway@gmail.com>
2025-02-17 21:20:27 -0500

    Testing
    Testing

--- Client/portfolio/src/pages/aidenPages/aidenHomePage/aidenHome.js
+++ Client/portfolio/src/pages/aidenPages/aidenHomePage/aidenHome.js
@@ -10,7 +10,7 @@ function AidenHome() {
       <header className="home-header">
       <h1><Link to = "/"><button className="standardButton">Return to home page</button></Link></h1>   
       <h2><Link to = "https://google.com"><button className="standardButton">Google</button></Link></h2>
-      <h2><Link to = "https://www.linkedin.com/in/aiden-dunaway"><button className="standardButton">Linkedin(TEST)</button></Link></h2>
+      <h2><Link to = "https://www.linkedin.com/in/aiden-dunaway"><button className="standardButton">Linkedin(TawdEST)</button></Link></h2>
       </header>
       </div>
   );
